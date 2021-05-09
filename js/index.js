7042f16f42f626b6e6eff0ba5cb14f664debf70a
Yuriy <yfrolou@mail.ru>
2021-05-10 01:04:18 +0300

    added lesson-7

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 const header_burger = document.querySelector(".header__burger");
 const menu = document.querySelector(".menu");
 const closeButton = document.getElementById("close");
+const toProductArr = document.querySelectorAll(".products__cards-image");
 
 header_burger.addEventListener("click", showMenu);
 closeButton.addEventListener("click", showMenu);
 
+toProductArr.forEach(function (item) {
+    item.addEventListener("click", goToPage);
+});
 
 function showMenu() {
     let shown = window.getComputedStyle(menu).display;
@@ -13,5 +17,11 @@ function showMenu() {
         menu.style.display = "block";
     } else {
         menu.style.display = "none";
+    }
+}
+
+function goToPage(event) {
+    if (event.target.className === "products__cards-image") {
+        document.location.href = "product.html";
     }
 }
