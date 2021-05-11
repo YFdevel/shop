d4058713438a8fe2966bbe15abaa88b797f74185
Yuriy <yfrolou@mail.ru>
2021-05-11 21:32:32 +0300

    modified dir awesome

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const header_burger = document.querySelector(".header__burger");
 const menu = document.querySelector(".menu");
-const closeButton = document.getElementById("close");
+const closeMenu = document.getElementById("close-menu");
 const toProductArr = document.querySelectorAll(".products__cards-image");
 
 header_burger.addEventListener("click", showMenu);
-closeButton.addEventListener("click", showMenu);
+closeMenu.addEventListener("click", showMenu);
 
 toProductArr.forEach(function (item) {
     item.addEventListener("click", goToPage);
